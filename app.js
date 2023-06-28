55e0bba4b1faef6264c703618651622977994e79
Code-X <andrewomosh44@gmail.com>
2023-06-28 12:05:22 +0300

    add ejs & starter pages

--- app.js
+++ app.js
@@ -1,34 +1,46 @@
 const express = require('express');
 
+// express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 // listen for requests
-app.listen(3000);
+app.listen(4000);
 
 
 app.get('/', (req, res) => {
     // automatically sets the content headers & status codes
     // res.send('<p>home page</p>');
-    res.sendFile('./views/index.html', { root: __dirname})
+    // res.sendFile('./views/index.html', { root: __dirname})
+    res.render('index')
 });
 
 
 app.get('/about', (req, res) => {
     // automatically sets the content headers & status codes
     // res.send('<p>home page</p>');
-    res.sendFile('./views/about.html', { root: __dirname})
+    // res.sendFile('./views/about.html', { root: __dirname})
+    res.render('about')
 });
 
 
-// redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create')
 });
 
 
-// 404
+// // redirects
+// app.get('/about-us', (req, res) => {
+//     res.redirect('/about');
+// });
+
+
+// // 404
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname})
+    // res.status(404).sendFile('./views/404.html', { root: __dirname})
+    res.status(404).render('404')
 });
 
 
